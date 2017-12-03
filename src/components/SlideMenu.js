e404eb29003bea47846df05a8c6962d5f1f33a3a
Brandon <wowbob396@gmail.com>
2017-12-02 23:27:44 -0600

    added toggle menu function

--- src/components/SlideMenu.js
+++ src/components/SlideMenu.js
@@ -2,10 +2,48 @@ import React, { Component } from 'react';
 import '../styles/SlideMenu.css';
 
 export class SlideMenu extends Component {
+
+  constructor(props, context) {
+    this.state = {
+      visible: false;
+    }
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+  }
+
+  handleMouseDown(e) {
+    this.toggleMenu();
+
+    console.log("Clicked!");
+    e.stopPropagation();
+  }
+  toggleMenu() {
+    this.setState(
+      {
+        visible: !this.state.visible;
+      }
+    );
+  }
   render() {
     return (
-      <div className="SlideMenu">
-
+      <div className="container">
+        <div>
+          <ul>
+            <li>
+              My Profile
+            </li>
+            <li>
+              Goals
+            </li>
+            <li>
+              Settings
+            </li>
+            <li>
+              Report Bug
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
