2758980ee324f22b600e29f5435b01eefdc80432
Brandon <wowbob396@gmail.com>
2017-12-06 19:08:55 -0600

    updated goals.js with an array now

--- frontend/src/components/Goals.js
+++ frontend/src/components/Goals.js
@@ -5,7 +5,10 @@ import '../styles/Goals.css';
 export class Goals extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: '',
+      goals: Array(10).fill(null),
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,9 +26,9 @@ export class Goals extends Component {
     return(
       <form onSubmit={this.handleSubmit} className="goal-submit">
         <div className="form-group">
-          <label>Goal:  &nbsp;&nbsp;
+          <label className="goal-label">Goal:        </label> &nbsp;&nbsp;
                 <input type="text" value={this.state.value} onChange={this.handleChange}/>
-          </label>
+
           &nbsp;&nbsp;   <input className="btn btn-default" type="submit" value="Submit"/>
         </div>
       </form>
