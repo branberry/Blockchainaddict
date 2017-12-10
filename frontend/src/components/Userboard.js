1de693c07879c92e4f8f7f4f127b00e944407d39
Brandon <wowbob396@gmail.com>
2017-12-09 19:20:04 -0600

    created a chart :D

--- frontend/src/components/Userboard.js
+++ frontend/src/components/Userboard.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
-import Dashboard from 'react-dazzle';
 import { Goals } from './Goals.js';
+import { Bar, Line, Pie } from 'react-chartjs-2';
 import '../styles/UserBoard.css';
 
 export class Userboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        chartData:{
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    }
+  }
   render() {
     return (
       <div className="dashboard">
         <h1> Your Dashboard </h1>
          <div>
            <Goals/>
+           <Bar
+             data={this.state.chartData}
+             options={{
+               maintainAspectRatio: false
+             }} />
+
         </div>
       </div>
     );
