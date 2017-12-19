5d4a043652df5f5067cd9b98d8bed5113940e968
Brandon <wowbob396@gmail.com>
2017-12-18 21:19:22 -0600

    created the CardanoData model

--- backend/cryptocurrency/models.py
+++ backend/cryptocurrency/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class CardanoData(models.Model):
+    price = models.FloatField(max_length=200)
+    pub_date = models.DateTimeField('Date published')
+
