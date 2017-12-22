8b16fd7e89fa3c3510bb359fc74ffa684a69d697
Brandon <wowbob396@gmail.com>
2017-12-22 10:41:39 -0600

    added access to db and some test queries

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host : 'DESKTOP-J8I4H9P',
+    user : 'wowbob396',
+    password : '*******',
+    database : 'lifeartificer'
+});
+
+connection.connect();
+connection.query
+connection.query('SELECT * FROM ada_data', (error,results,fields) => {
+    if (error) throw error;
+    console.log(results);
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/hello',(req,res) => res.send('HAHAHA!'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
